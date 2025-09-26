c85aa86853ac954be36531c648403beba1ab01af
Hannigan Alderson <hannigan.a0@gmail.com>
2025-09-26 10:25:57 -0700

    Hello World

---
+++ cplorations/c00/main.c
@@ -0,0 +1,13 @@
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * [NAME] Hannigan Alderson
+ * [TERM] FALL 2025
+ *****************************************/
+#include <stdio.h>
+int main(void) {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
